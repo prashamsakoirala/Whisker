0b612cefb5a910c7c31d7da2fe21ddb5d89bfa28
prashamsakoirala <pkoirala04@gmail.com>
2025-10-14 15:40:59 -0600

    updated get current user

--- backend/users/dependencies.py
+++ backend/users/dependencies.py
@@ -1,31 +1,37 @@
-from fastapi import Cookie, Depends, HTTPException
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from sqlalchemy import and_, or_, not_
 import jwt
 from jwt import PyJWTError
 from database import get_db
-from models.user_model import User, Partnership
-from models.cat_model import *
 from auth.config import SECRET_KEY, ALGORITHM
-from auth.tokens import create_access_token
-from schemas.token_schema import *
+from users.crud import get_user
 
 # get_current_user
+# What does the following do?
 
+# Checks to make sure that there is an Authorization header with a valid JWT token in the request
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     try:
+        # Get payload from token
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # Get User ID from payload
         user_id = payload.get("sub")
-        if user_id is None or payload.get("scope") != "access_token":
-            raise HTTPException(status_code=401, detail="Invalid token")
+        # If user_id is None
+        if user_id is None:
+            raise ValueError("Invalid token: missing user ID")
+
+        # If scope is not access_token
+        if payload.get("scope") != "access_token":
+            raise ValueError("Invalid token: incorrect scope")
+        
     except PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+        raise ValueError("Invalid token")
 
     # TODO use crud operations for the following instead of direct database access
-    user = db.query(User).filter(User.id == user_id).first()
+    user = get_user(db = db, user_id=user_id)
     if not user:
-        raise HTTPException(status_code=401, detail="User not found")
+        raise ValueError("User not found")
     return user
